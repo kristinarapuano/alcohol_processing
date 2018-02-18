4dd99ab25f8edbc044965cda5596a951ce0d361c
kristinarapuano <kristinarapuano@gmail.com>
2018-02-18 17:08:22 -0500

    updated to regress out outliers

--- nuissanceregress_alcohol.py
+++ nuissanceregress_alcohol.py
@@ -10,9 +10,12 @@ from nltools.data import Brain_Data
 from nltools.mask import expand_mask, collapse_mask
 from scipy import stats
 import argparse
-from subprocess import call
+import nibabel as nib
+from nilearn.masking import compute_epi_mask, apply_mask, unmask
+from nilearn.plotting import plot_stat_map
+#from subprocess import call
 
-base_dir = '/idata/lchang/rapuanok/alcohol2_bidsDS/'
+base_dir = '/dartfs/rc/lab/D/DBIC/cosanlab/datax/Projects/alcohol/'
 mask = Brain_Data('http://neurovault.org/media/images/2099/Neurosynth%20Parcellation_0.nii.gz')
 mask_x = expand_mask(mask)
 
@@ -56,12 +59,12 @@ for ses in sessions:
         alldat = alldat.append(dat)
         
     maskeddat = alldat.apply_mask(nib.load(wb_mask))
-    mn = np.mean(maskeddat, axis=0)
-    sd = np.std(maskeddat, axis=0)
+    mn = np.mean(maskeddat.data, axis=0)
+    sd = np.std(maskeddat.data, axis=0)
     tsnr = np.true_divide(mn, sd)
     # Compute mean across voxels within each TR
-    global_mn = np.mean(maskeddat, axis=1)
-    global_sd = np.std(maskeddat, axis=1)
+    global_mn = np.mean(maskeddat.data, axis=1)
+    global_sd = np.std(maskeddat.data, axis=1)
     # Unmask data for plotting below
     mn = unmask(mn, wb_mask)
     sd = unmask(sd, wb_mask)
@@ -72,45 +75,47 @@ for ses in sessions:
                                np.where(global_mn < np.mean(global_mn) - np.std(global_mn) * 3))
 
     # Identify frame difference outliers
-    frame_diff = np.mean(np.abs(np.diff(masked_data, axis=0)), axis=1)
+    frame_diff = np.mean(np.abs(np.diff(maskeddat.data, axis=0)), axis=1)
     frame_outliers = np.append(np.where(frame_diff > np.mean(frame_diff) + np.std(frame_diff) * 3),
                               np.where(frame_diff < np.mean(frame_diff) - np.std(frame_diff) * 3))
 
-    fd_file_name = "fd_outliers.txt"
-    global_file_name = "global_outliers.txt"
+    fd_file_name = join(sub_dir, "fd_outliers.txt")
+    global_file_name = join(sub_dir, "global_outliers.txt")
     np.savetxt(fd_file_name, frame_outliers)
     np.savetxt(global_file_name, global_outliers)
     
-    rps = sorted(glob.glob(join(base_dir,'derivatives','fmriprep', sub, ses, 'func', '*_confounds.tsv')))
+    confound_fns = sorted(glob.glob(join(base_dir,'derivatives','fmriprep', sub, ses, 'func', '*_confounds.tsv')))
 
-    allrps = []
-    for rp in rps:
-        ra = pd.read_table(rp, skipinitialspace=True, usecols=
-                #['aCompCor0' + str(x) for x in range(0,6)] +
-                ['X', 'Y', 'Z','RotX', 'RotY', 'RotZ'])
-        allrps.append(ra)
+    all_confounds = []
+    for fn in confound_fns:
+        confounds = pd.read_table(rp, skipinitialspace=True, usecols=
+                    ['aCompCor0' + str(x) for x in range(0,6)] +
+                    ['X', 'Y', 'Z','RotX', 'RotY', 'RotZ'])
+        all_confounds.append(confounds)
 
-    rpstack = pd.concat(allrps)
-    rpstack = rpstack-rpstack.mean() #mean center
-    rpstack[['rpsq' + str(x) for x in range(1,7)]] = rpstack.iloc[:,-6:]**2 #add squared
-    rpstack[['rpdiff' + str(x) for x in range(1,7)]] = pd.DataFrame(rpstack.iloc[:,-6:][rpstack.iloc[:,-6:].columns[0:6]].diff()) #derivative
-    rpstack[['rpdiffsq' + str(x) for x in range(1,7)]] = pd.DataFrame(rpstack.iloc[:,-6:][rpstack.iloc[:,-6:].columns[0:6]].diff())**2 #derivatives squared
-    rpstack['Intercept'] = 1 # Add Intercept
-    rpstack['LinearTrend'] = range(rpstack.shape[0])-np.mean(range(rpstack.shape[0])) # Add Linear Trend
-    rpstack['QuadraticTrend'] = rpstack['LinearTrend']**2
-    rpstack = rpstack.fillna(value=0) # fill "diff" NAs w/0 (first row)
-    rpstack['FrameOutliers'] = frame_outliers
-    rpstack['GlobalOutliers'] = global_outliers    
+    confound_regs = pd.concat(all_confounds)
+    confound_regs = confound_regs-confound_regs.mean() #mean center
+    confound_regs[['rpsq' + str(x) for x in range(1,7)]] = confound_regs.iloc[:,-6:]**2 #add squared
+    confound_regs[['rpdiff' + str(x) for x in range(1,7)]] = pd.DataFrame(confound_regs.iloc[:,-6:][confound_regs.iloc[:,-6:].columns[0:6]].diff()) #derivative
+    confound_regs[['rpdiffsq' + str(x) for x in range(1,7)]] = pd.DataFrame(confound_regs.iloc[:,-6:][confound_regs.iloc[:,-6:].columns[0:6]].diff())**2 #derivatives squared
+    confound_regs['Intercept'] = 1 # Add Intercept
+    confound_regs['LinearTrend'] = range(confound_regs.shape[0])-np.mean(range(confound_regs.shape[0])) # Add Linear Trend
+    confound_regs['QuadraticTrend'] = confound_regs['LinearTrend']**2
+    confound_regs = confound_regs.fillna(value=0) # fill "diff" NAs w/0 (first row)
+    zeros = np.zeros(len(maskeddat))
+    zeros[frame_outliers] = 1
+    confound_regs['FrameOutliers'] = zeros
+    zeros = np.zeros(len(maskeddat))
+    zeros[global_outliers] = 1
+    confound_regs['GlobalOutliers'] = zeros  
         
-    maskeddat.X = rpstack # Add to dat
+    maskeddat.X = confound_regs # Add to dat
     maskeddat_reg = maskeddat.regress()
-    maskeddat_reg['residual'].write(join(base_dir, 'derivatives',
-            'roi_cleaned', sub,'{0}_{1}_wholebrain_4mm_resid.nii.gz'.format(sub,ses)))
+    maskeddat_reg['residual'].write(join(sub_dir,'{0}_{1}_wholebrain_4mm_resid.nii.gz'.format(sub,ses)))
 
     for roi,m in enumerate(mask_x):
         maskeddat = alldat.apply_mask(m)
         maskeddat.X = rpstack0 # Add to dat
         maskeddat_reg = maskeddat.regress()
-
-        pd.DataFrame(maskeddat_reg['residual'].data).to_csv(join(base_dir, 'derivatives',
-                        'roi_cleaned', sub, '{0}_{1}_ROI{2}_resid.csv'.format(sub,ses,roi)),index=None)
+        pd.DataFrame(maskeddat_reg['residual'].data).to_csv(join(sub_dir,
+                                                   '{0}_{1}_ROI{2}_resid.csv'.format(sub,ses,roi)),index=None)
